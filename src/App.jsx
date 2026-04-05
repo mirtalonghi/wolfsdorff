9c2ff3647509b45788860427a4ee4ba43b9a5970
Mirta <mirta.wolfsdorf@gmail.com>
2026-04-05 16:22:14 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+export default function App() {
+  return (
+   <main>
+    {/*Gradient image */}
+    <img className="absolute top-0 right-0
+    opacity-60 -z-10" src="/gradient.png" 
+    alt="Gradient-img" /> 
+    
+    {/*blur Effect */}
+    <div className="h-0 w-[40rem] absolute top-
+    [20% right-[-5%] shadow-
+    [0_0_900px_20px_#e99b63] -rotate-[30deg]
+     -z-10"></div>
+   <Header/>
+   
+  </main>
+  )
+}
