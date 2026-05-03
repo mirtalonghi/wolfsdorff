c27f0f317ccecb9b24a84efe1d48cb7b0fa26f25
Ivan Wolfsdorf <ivan@valere.io>
2026-05-03 19:20:37 +0000

    added more students

--- src/data/legacyAlumni.js
+++ src/data/legacyAlumni.js
@@ -6,7 +6,16 @@
 export const legacyAlumni = [
   {
     id: 'vico-doig',
-    name: 'Vico Doig',
+    name: 'Vico Doig Carpena',
     instagramUrl: 'https://www.instagram.com/vicodoig/',
+  },
+  {
+    id: 'doris-maria-sumar-kattan',
+    name: 'Doris Maria Sumar Kattan',
+    instagramUrl: 'https://www.instagram.com/doris_sumark/',
+  },
+  {
+    id: 'bert-engelhard',
+    name: 'Bert Engelhard',
   },
 ];
